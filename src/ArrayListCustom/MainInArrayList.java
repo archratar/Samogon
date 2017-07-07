1ada461deeebed25892e04a948bb3bcec1191a1d
Vasil Hlukhenkyi <tripylec@gmail.com>
2017-07-07 23:31:11 +0300

    Added new package ArrayListCustom for implementing standard java Collection class ArrauList, build essential structure of data types

---
+++ src/ArrayListCustom/MainInArrayList.java
@@ -0,0 +1,10 @@
+package ArrayListCustom;
+
+/**
+ * Created by arch on 7/7/17.
+ */
+public class MainInArrayList {
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/ArrayListCustom/Overload.java
@@ -0,0 +1,7 @@
+package ArrayListCustom;
+
+/**
+ * Created by arch on 7/7/17.
+ */
+public @interface Overload {
+}
